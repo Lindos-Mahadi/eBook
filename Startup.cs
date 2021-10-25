8656597cadb1fc7e16eb1205e12a71548270a1e3
Lindos-Mahadi <lindosmahadi@.com>
2021-10-25 17:15:37 +0600

    all Middleware app.Use(), app.Next(), app.Map() & Http Pipline

--- Startup.cs
+++ Startup.cs
@@ -72,13 +72,13 @@ namespace eBook
                 await next();
                 // Do logging or other work that doesn't write to the Response.
             });
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapControllerRoute(
-            //        name: "default",
-            //        pattern: "{controller=Home}/{action=Index}/{id?}");
-            //    endpoints.MapRazorPages();
-            //});
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
         }
     }
 }
