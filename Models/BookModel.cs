3f2fbd64ab2e0589f95e1e793bc0b69975fa8e3a
Lindos-Mahadi <lindosmahadi@.com>
2021-11-02 10:24:53 +0600

    Display All BookList i GetAllBooks page & Changed in Layout page

--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -12,5 +12,6 @@ namespace eBook.Models
         public string Title { get; set; }
 
         public string Author { get; set; }
+        public string Description { get; set; }
     }
 }
--- Repository/BookRepository.cs
+++ Repository/BookRepository.cs
@@ -27,11 +27,18 @@ namespace eBook.Repository
         {
             return new List<BookModel>()
             {
-                new BookModel(){Id =1, Title="MVC", Author = "Nitish" },
-                new BookModel(){Id =2, Title="Dot Net Core", Author = "Nitish" },
-                new BookModel(){Id =3, Title="C#", Author = "Monika" },
-                new BookModel(){Id =4, Title="Java", Author = "Webgentle" },
-                new BookModel(){Id =5, Title="Php", Author = "Webgentle" },
+                new BookModel(){Id =1, Title="MVC", Author = "lindos" , Description="This is the description for MVC"},
+                new BookModel(){Id =2, Title="Dot Net Core", Author = "lindos" , Description="This is the description for DotNet Core"},
+                new BookModel(){Id =3, Title="C#", Author = "lindos" , Description="This is the description for C#"},
+                new BookModel(){Id =4, Title="Java", Author = "lindos" , Description="This is the description for Java"},
+                new BookModel(){Id =5, Title="Php", Author = "lindos", Description="This is the description for PHP"},
+                new BookModel(){Id =6, Title="Azure", Author = "lindos" , Description="This is the description for Azure"},
+                new BookModel(){Id =7, Title="Angular", Author = "lindos" , Description="This is the description for Angular"},
+                new BookModel(){Id =8, Title="F#", Author = "lindos" , Description="This is the description for F#"},
+                new BookModel(){Id =9, Title="React", Author = "lindos" , Description="This is the description for React"},
+                new BookModel(){Id =10, Title="Python", Author = "lindos", Description="This is the description for Python"},
+                new BookModel(){Id =11, Title="DevOps", Author = "lindos", Description="This is the description for DevOps"},
+                new BookModel(){Id =12, Title="Networking", Author = "lindos", Description="This is the description for Networking"},
             };
         }
     }
