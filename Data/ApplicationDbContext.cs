a4b719f41eaec635742f2e8d86d0f92d261d3970
Lindos-Mahadi <lindosmahadi@.com>
2021-11-03 12:17:09 +0600

    SetUp ApplicationDbContext Class & Created Books class for  Domain

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using eBook.Data.DomainModals;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -6,11 +7,13 @@ using System.Text;
 
 namespace eBook.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Books> Books { get; set; }
     }
 }
