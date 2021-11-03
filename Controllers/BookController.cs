0d726fd62c74d15e597cb196c123047fc738cd6b
Lindos-Mahadi <lindosmahadi@.com>
2021-11-03 20:03:07 +0600

    SelectListGroup

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -38,7 +38,19 @@ namespace eBook.Controllers
         [HttpGet]
         public ActionResult AddNewBook(bool isSuccess = false, int bookId = 0 )
         {
-            ViewBag.Language = new SelectList(GetLanguage(), "Id", "Text");
+            var group1 = new SelectListGroup() { Name = "Group 1" };
+            var group2 = new SelectListGroup() { Name = "Group 2", Disabled = true };
+            var group3 = new SelectListGroup() { Name = "Group 3" };
+
+            ViewBag.Language = new List<SelectListItem>()
+            {
+                new SelectListItem(){Text = "Hindi", Value = "1", Group = group1 },
+                new SelectListItem(){Text = "English", Value = "2", Group = group1 },
+                new SelectListItem(){Text = "Dutch", Value = "3", Group = group2},
+                new SelectListItem(){Text = "Tamil", Value = "4", Group = group2 },
+                new SelectListItem(){Text = "Urdu", Value = "5" , Group = group3},
+                new SelectListItem(){Text = "Chinese", Value = "6", Group = group3 },
+            };
 
             ViewBag.IsSuccess = isSuccess;
             ViewBag.BookId = bookId;
