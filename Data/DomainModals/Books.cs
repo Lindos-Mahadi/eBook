f7d829df5e65118d4b770c664c9d6fefcab94989
Lindos-Mahadi <lindosmahadi@.com>
2021-11-05 19:44:53 +0600

    Client Side Validation is Required

--- Data/DomainModals/Books.cs
+++ Data/DomainModals/Books.cs
@@ -9,14 +9,19 @@ namespace eBook.Data.DomainModals
     public class Books
     {
         public int Id { get; set; }
+        [StringLength(100, MinimumLength = 5)]
+        [Required(ErrorMessage = "Please enter the title of your book")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Please enter the author name")]
         public string Author { get; set; }
+        [StringLength(500)]
         public string Description { get; set; }
         public string Category { get; set; }
 
         public int LanguageId { get; set; }
         public Language Language { get; set; }
 
+        [Display(Name = "Total pages of book")]
         public int TotalPages { get; set; }
         public DateTime? CreatedOn { get; set; }
         public DateTime? UpdatedOn { get; set; }
--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -9,20 +9,17 @@ namespace eBook.Models
     public class BookModel
     {
         public int Id { get; set; }
-        //[StringLength(100, MinimumLength = 5)]
-        //[Required(ErrorMessage = "Please enter the title of your book")]
-        [CustomValidation("azure")]
         public string Title { get; set; }
-        [Required(ErrorMessage = "Please enter the author name")]
+        
         public string Author { get; set; }
-        [StringLength(500)]
+        
         public string Description { get; set; }
         public string Category { get; set; }
 
         public int LanguageId { get; set; }
         public string Language { get; set; }
 
-        [Display(Name = "Total pages of book")]
+        
         public int? TotalPages { get; set; }
     }
 }
