6abeae7d5e2d48c99bb96cd62ac0ebf0e77eb48e
Lindos-Mahadi <lindosmahadi@.com>
2021-11-09 19:27:49 +0600

    Login in asp.net core

--- Repository/AccountRepository.cs
+++ Repository/AccountRepository.cs
@@ -10,10 +10,13 @@ namespace eBook.Repository
     public class AccountRepository : IAccountRepository
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountRepository(UserManager<ApplicationUser> userManager)
+        public AccountRepository(UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public async Task<IdentityResult> CreateUserAsync(SignUpUserModel userModel)
@@ -28,6 +31,10 @@ namespace eBook.Repository
             };
             var result = await _userManager.CreateAsync(user, userModel.Password);
             return result;
+        }
+        public async Task<SignInResult> PasswordSignInAsync(SignInModel signInModel)
+        {
+            return await _signInManager.PasswordSignInAsync(signInModel.Email, signInModel.Password, signInModel.RememberMe, false);
         }
     }
 }
--- Repository/IAccountRepository.cs
+++ Repository/IAccountRepository.cs
@@ -7,5 +7,6 @@ namespace eBook.Repository
     public interface IAccountRepository
     {
         Task<IdentityResult> CreateUserAsync(SignUpUserModel userModel);
+        Task<SignInResult> PasswordSignInAsync(SignInModel signInModel);
     }
 }
